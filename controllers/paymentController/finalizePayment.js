7ba293a47e4a0f537c68b4c903d999482d37c568
MateuszSlezal <M4teks93@gmail.com>
2025-01-07 22:03:20 +0100

    ss

--- controllers/paymentController/finalizePayment.js
+++ controllers/paymentController/finalizePayment.js
@@ -6,9 +6,12 @@ export const finalizePayment  = (req, res) => {
 
   console.log('FINALIZE PAYMENT', req.body)
    const sign = generateFinalizePaymentSign(sessionId, orderId, amount)
-   console.log('wysłane przez p24:', req.body)
-   markPaymentAsPaid(sessionId)
+  // console.log('wysłane przez p24:', req.body)
+   console.log('sessionId')
+   const payment = markPaymentAsPaid(sessionId)
+   console.log('platnosc po sfinalizowaniu:', payment)
 
-  const payment = verifyTransaction({ sessionId, amount, orderId, sign });  
-  console.log('platnosc po sfinalizowaniu:', payment)
+   verifyTransaction({ sessionId, amount, orderId, sign });  
+  console.log('platnosc po sfinalizowaniu:')
+  res.status(201).json({ message: "Transakcja utworzona pomyślnie", });
 }
