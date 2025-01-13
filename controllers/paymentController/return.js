1cbd6b1a59f9a9fb298eb28fa8f4cf8ca7604330
MateuszSlezal <M4teks93@gmail.com>
2025-01-13 18:20:44 +0100

    add

--- controllers/paymentController/return.js
+++ controllers/paymentController/return.js
@@ -19,9 +19,9 @@ import { findPaymentById } from "../../services/dataServices.js";
 
 
 export const redirectUser = (req, res) => {
-  const transactionId = req.params.transactionId;
-  const transaction = findPaymentById(transactionId);
-
+ // const transactionId = req.params.transactionId;
+ // const transaction = findPaymentById(transactionId);
+const url = "https://fundacja-hematologiczna.github.io/gala/#/rejestracja";
   res.redirect(`${url}`)
 
   // if (!transaction) {
@@ -36,4 +36,26 @@ export const redirectUser = (req, res) => {
   //   res.redirect(`${url}?status=failure`);
   // }
 };
+
+// export const redirectUser = async (req, res) => {
+//   try {
+//     const transactionId = req.params.transactionId;
+//     const transaction = await findPaymentById(transactionId); // Sprawdź, czy funkcja jest asynchroniczna
+
+//     if (!transaction) {
+//       return res.redirect(`${url}?status=error`);
+//     }
+
+//     const url = transaction.urlReturn;
+
+//     if (transaction.paid) {
+//       return res.redirect(`${url}?status=success`);
+//     } else {
+//       return res.redirect(`${url}?status=failure`);
+//     }
+//   } catch (error) {
+//     console.error("Błąd w redirectUser:", error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// };
 
